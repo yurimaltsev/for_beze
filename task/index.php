3250ed63b29654b9b5f7976f1fa55f228aa80522
yurimaltsev <gorkytx@mail.ru>
2024-04-24 02:21:28 +0500

    Тестовое задание

--- task/index.php
+++ task/index.php
@@ -1,10 +1,7 @@
 <?php
 
 require_once 'Lessi.php';
-try {
-    $repository = new Lessi();
-    $repository->saveAnimal('ostrich')->saveAnimal('dolphin');
-    $repository->doAction('fly');
-} catch (\Throwable $e) {
-    var_dump($e);
-}
+
+$repository = new Lessi();
+$repository->saveAnimal('ostrich')->saveAnimal('dolphin');
+$repository->doAction('fly');
